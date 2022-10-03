3a1a6b991a57a1dbc79b7be712d8ddf0f1fd0cb5
Yuri <yuriqpaiva@gmail.com>
2022-10-02 22:41:16 -0300

    Add account creation route

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
-const express = require("express");
+const express = require('express');
+const { v4: uuidV4 } = require('uuid');
 
 const app = express();
 
+app.use(express.json());
+
+const customers = [];
+
+app.post('/account', (request, response) => {
+  const { cpf, name } = request.body;
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: 'Customer already exists' });
+  }
+
+  customers.push({
+    id: uuidV4(),
+    cpf,
+    name,
+    statement: [],
+  });
+
+  return response.status(201).send();
+});
+
 app.listen(3333, () => {
-  console.log("Listening...");
+  console.log('Listening...');
 });
